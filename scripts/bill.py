23bba55e082a9fc51f6b303fd88d0d0e15a93974
malinkang <malinkang@adsconflux.com>
2024-01-28 21:00:08 +0800

    fix bug

--- scripts/bill.py
+++ scripts/bill.py
@@ -1,41 +1,162 @@
+import argparse
+import csv
+from datetime import datetime
 import os
 import re
+import pendulum
 import requests
-from github import Github
 from zipfile import ZipFile
+from notion_helper import NotionHelper
+from utils import (
+    get_date,
+    get_icon,
+    get_number,
+    get_relation,
+    get_rich_text,
+    get_title,
+)
 
-# Initialize GitHub API client
-g = Github(os.getenv('GITHUB_TOKEN'))
-repo = g.get_repo(os.getenv('GITHUB_REPOSITORY'))
-issue_number = os.environ.get('ISSUE_NUMBER') or os.environ.get('GITHUB_REF').split('/')[-1]
-issue = repo.get_issue(number=int(issue_number))
 
-# Find zip file URL in the issue body or comments
-zip_url = None
-if re.search(r'https?://\S+\.zip', issue.body):
-    zip_url = re.search(r'https?://\S+\.zip', issue.body).group()
+def donwload_zip():
+    zip_url = None
+    if re.search(r"https?://\S+\.zip", body):
+        zip_url = re.search(r"https?://\S+\.zip", body).group()
+    results = []
+    if zip_url:
+        # Download the zip file
+        headers = {
+            "Authorization": f'{os.getenv("GITHUB_TOKEN")}',
+            "Accept": "application/octet-stream",
+        }
+        print(f"zip url = {zip_url}")
+        r = requests.get(zip_url, headers=headers)
+        zip_path = "downloaded.zip"
+        with open(zip_path, "wb") as zip_file:
+            zip_file.write(r.content)
 
-if not zip_url:
-    for comment in issue.get_comments():
-        if re.search(r'https?://\S+\.zip', comment.body):
-            zip_url = re.search(r'https?://\S+\.zip', comment.body).group()
-            break
+        # Unzip the file using the password from secrets
+        with ZipFile(zip_path) as zip_ref:
+            zip_ref.extractall(pwd=os.getenv("ZIP_PASSWORD").encode())
+            # Print the list of file names
+            print("Contents of the zip file:")
+            for file_info in zip_ref.infolist():
+                file_path = os.path.abspath(file_info.filename)
+                if file_path.endswith(".csv"):
+                    results.extend(parse_csv(file_path))
 
-if zip_url:
-    # Download the zip file
-    r = requests.get(zip_url)
-    zip_path = 'downloaded.zip'
-    with open(zip_path, 'wb') as zip_file:
-        zip_file.write(r.content)
+        print("Zip file downloaded and extracted.")
+    else:
+        print("No zip file URL found in issue body or comments.")
+    return results
 
-    # Unzip the file using the password from secrets
-    with ZipFile(zip_path) as zip_ref:
-        zip_ref.extractall(pwd=os.getenv('ZIP_PASSWORD').encode())
-        # Print the list of file names
-        print('Contents of the zip file:')
-        for file_info in zip_ref.infolist():
-            print(file_info.filename)
 
-    print('Zip file downloaded and extracted.')
-else:
-    print('No zip file URL found in issue body or comments.')
+encodings = ["utf-8", "gbk"]
+
+
+def parse_csv(file):
+    results = []
+    for encoding in encodings:
+        try:
+            with open(file, encoding=encoding) as csvfile:
+                reader = csv.reader(csvfile)
+                header = None
+                for index, row in enumerate(reader):
+                    d = {}
+                    if "交易时间" in row:
+                        header = row
+                        print(header)
+                        continue
+                    if header != None:
+                        for index, column in enumerate(header):
+                            d[column] = row[index]
+                        results.append(d)
+
+                break
+        except UnicodeDecodeError as e:
+            print(f"尝试使用编码 {encoding} 读取时出错: {e}")
+    return results
+
+
+def create_page(page_id,date, type, payee, product, amount, price, note, method,no):
+    payee = notion_helper.get_relation_id(
+        payee,
+        notion_helper.payee_database_id,
+        "https://www.notion.so/icons/shop_gray.svg",
+    )
+    method = notion_helper.get_relation_id(
+        method,
+        notion_helper.method_database_id,
+        "https://www.notion.so/icons/credit-card_gray.svg",
+    )
+    category = notion_helper.get_relation_id(
+        type,
+        notion_helper.category_database_id,
+        "https://www.notion.so/icons/kind_gray.svg",
+    )    
+    amount = notion_helper.get_relation_id(
+        amount,
+        notion_helper.income_database_id,
+        "https://www.notion.so/icons/kind_gray.svg",
+    )
+    properties = {
+        "日期": get_date(start=date),
+        "分类": get_relation([category]),
+        "商品": get_title(product),
+        "商家": get_relation([payee]),
+        "支付方式": get_relation([method]),
+        "收/支": get_relation([amount]),
+        "金额(元)": get_number(price),
+        "备注": get_rich_text(note),
+        "交易单号": get_rich_text(no),
+        "交易单号": get_rich_text(no),
+    }
+    notion_helper.get_date_relation(properties,pendulum.parse(date))
+    parent = {"database_id": notion_helper.bill_database_id, "type": "database_id"}
+    if page_id:
+        pass
+    else:    
+        notion_helper.create_page(
+            parent=parent,
+            properties=properties,
+            icon=get_icon("https://www.notion.so/icons/cash_gray.svg"),
+        )
+
+def check(transaction_id):
+    filter = { 
+        "property": "交易单号",
+        "rich_text": {"equals": transaction_id},
+    }
+    results = notion_helper.query(database_id=notion_helper.bill_database_id,filter=filter)
+    if results.get("results"):
+        return results["results"][0]["id"]
+    else:
+        return None
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    notion_helper = NotionHelper()
+    parser.add_argument("body")
+    options = parser.parse_args()
+    body = options.body
+    results = donwload_zip()
+    for result in results:
+        amount = result.get("收/支")
+        if amount != "收入" and amount != "支出":
+            continue
+        date = result.get("交易时间")
+        type = result.get("交易类型") if result.get("交易类型") else result.get("交易分类")
+        product = result.get("商品") if result.get("商品") else result.get("商品说明")
+        price = result.get("金额(元)") if result.get("金额(元)") else result.get("金额")
+        price = float(price.replace("¥", ""))
+        if(amount=="支出"):
+            price = 0 - price
+        method = result.get("支付方式") if result.get("支付方式") else result.get("收/付款方式")
+        payee = result.get("交易对方")
+        no = result.get("交易单号") if result.get("交易单号") else result.get("交易订单号")
+        no = no.strip()
+        note = result.get("备注")
+        page_id = check(no)
+        print(f"交易单号{no} page_id={page_id}")
+        create_page(page_id,date, type, payee, product, amount, price, note, method,no)
+
+
---
+++ scripts/notion_helper.py
@@ -0,0 +1,220 @@
+import logging
+import os
+import re
+import time
+
+from notion_client import Client
+from retrying import retry
+from datetime import timedelta
+
+from utils import (
+    format_date,
+    get_date,
+    get_first_and_last_day_of_month,
+    get_first_and_last_day_of_week,
+    get_first_and_last_day_of_year,
+    get_icon,
+    get_number,
+    get_relation,
+    get_rich_text,
+    get_title,
+    timestamp_to_date,
+)
+
+TAG_ICON_URL = "https://www.notion.so/icons/tag_gray.svg"
+USER_ICON_URL = "https://www.notion.so/icons/user-circle-filled_gray.svg"
+TARGET_ICON_URL = "https://www.notion.so/icons/target_red.svg"
+BOOKMARK_ICON_URL = "https://www.notion.so/icons/bookmark_gray.svg"
+
+
+class NotionHelper:
+    database_name_dict = {
+        "BILL_DATABASE_NAME": "账单",
+        "METHOD_DATABASE_NAME": "支付方式",
+        "PAYEE_DATABASE_NAME": "商家",
+        "INCOME_DATABASE_NAME": "收支",
+        "CATEGORY_DATABASE_NAME": "分类",
+        "DAY_DATABASE_NAME": "日",
+        "WEEK_DATABASE_NAME": "周",
+        "MONTH_DATABASE_NAME": "月",
+        "YEAR_DATABASE_NAME": "年",
+    }
+    database_id_dict = {}
+    image_dict = {}
+
+    def __init__(self):
+        self.client = Client(auth=os.getenv("NOTION_TOKEN"), log_level=logging.ERROR)
+        self.__cache = {}
+        self.search_database(self.extract_page_id(os.getenv("NOTION_PAGE")))
+        for key in self.database_name_dict.keys():
+            if os.getenv(key) != None and os.getenv(key) != "":
+                self.database_name_dict[key] = os.getenv(key)
+        self.bill_database_id = self.database_id_dict.get(
+            self.database_name_dict.get("BILL_DATABASE_NAME")
+        )
+        self.method_database_id = self.database_id_dict.get(
+            self.database_name_dict.get("METHOD_DATABASE_NAME")
+        )
+        self.payee_database_id = self.database_id_dict.get(
+            self.database_name_dict.get("PAYEE_DATABASE_NAME")
+        )
+        self.income_database_id = self.database_id_dict.get(
+            self.database_name_dict.get("INCOME_DATABASE_NAME")
+        )
+        self.category_database_id = self.database_id_dict.get(
+            self.database_name_dict.get("CATEGORY_DATABASE_NAME")
+        )
+        self.day_database_id = self.database_id_dict.get(
+            self.database_name_dict.get("DAY_DATABASE_NAME")
+        )
+        self.week_database_id = self.database_id_dict.get(
+            self.database_name_dict.get("WEEK_DATABASE_NAME")
+        )
+        self.month_database_id = self.database_id_dict.get(
+            self.database_name_dict.get("MONTH_DATABASE_NAME")
+        )
+        self.year_database_id = self.database_id_dict.get(
+            self.database_name_dict.get("YEAR_DATABASE_NAME")
+        )
+
+    def extract_page_id(self, notion_url):
+        # 正则表达式匹配 32 个字符的 Notion page_id
+        match = re.search(
+            r"([a-f0-9]{32}|[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})",
+            notion_url,
+        )
+        if match:
+            return match.group(0)
+        else:
+            raise Exception(f"获取NotionID失败，请检查输入的Url是否正确")
+
+    def search_database(self, block_id):
+        children = self.client.blocks.children.list(block_id=block_id)["results"]
+        # 遍历子块
+        for child in children:
+            # 检查子块的类型
+
+            if child["type"] == "child_database":
+                self.database_id_dict[
+                    child.get("child_database").get("title")
+                ] = child.get("id")
+            elif child["type"] == "image":
+                self.image_dict["url"] = child.get("image").get("external").get("url")
+                self.image_dict["id"] = child.get("id")
+            # 如果子块有子块，递归调用函数
+            if "has_children" in child and child["has_children"]:
+                self.search_database(child["id"])
+
+    def update_image_block_link(self, block_id, new_image_url):
+        # 更新 image block 的链接
+        self.client.blocks.update(
+            block_id=block_id, image={"external": {"url": new_image_url}}
+        )
+
+    def get_relation_id(self, name, id, icon, properties={}):
+        key = f"{id}{name}"
+        if key in self.__cache:
+            return self.__cache.get(key)
+        filter = {"property": "标题", "title": {"equals": name}}
+        response = self.client.databases.query(database_id=id, filter=filter)
+        if len(response.get("results")) == 0:
+            parent = {"database_id": id, "type": "database_id"}
+            properties["标题"] = get_title(name)
+            page_id = self.client.pages.create(
+                parent=parent, properties=properties, icon=get_icon(icon)
+            ).get("id")
+        else:
+            page_id = response.get("results")[0].get("id")
+        self.__cache[key] = page_id
+        return page_id
+
+    @retry(stop_max_attempt_number=3, wait_fixed=5000)
+    def update_page(self, page_id, properties, icon):
+        return self.client.pages.update(
+            page_id=page_id, icon=icon, properties=properties
+        )
+
+    @retry(stop_max_attempt_number=3, wait_fixed=5000)
+    def create_page(self, parent, properties, icon):
+        return self.client.pages.create(parent=parent, properties=properties, icon=icon)
+
+    @retry(stop_max_attempt_number=3, wait_fixed=5000)
+    def query(self, **kwargs):
+        kwargs = {k: v for k, v in kwargs.items() if v}
+        return self.client.databases.query(**kwargs)
+
+    @retry(stop_max_attempt_number=3, wait_fixed=5000)
+    def delete_block(self, block_id):
+        return self.client.blocks.delete(block_id=block_id)
+
+    @retry(stop_max_attempt_number=3, wait_fixed=5000)
+    def query_all(self, database_id):
+        """获取database中所有的数据"""
+        results = []
+        has_more = True
+        start_cursor = None
+        while has_more:
+            response = self.client.databases.query(
+                database_id=database_id,
+                start_cursor=start_cursor,
+                page_size=100,
+            )
+            start_cursor = response.get("next_cursor")
+            has_more = response.get("has_more")
+            results.extend(response.get("results"))
+        return results
+
+    def get_week_relation_id(self, date):
+        year = date.isocalendar().year
+        week = date.isocalendar().week
+        week = f"{year}年第{week}周"
+        start, end = get_first_and_last_day_of_week(date)
+        properties = {"日期": get_date(format_date(start), format_date(end))}
+        return self.get_relation_id(
+            week, self.week_database_id, TARGET_ICON_URL, properties
+        )
+
+    def get_month_relation_id(self, date):
+        month = date.strftime("%Y年%-m月")
+        start, end = get_first_and_last_day_of_month(date)
+        properties = {"日期": get_date(format_date(start), format_date(end))}
+        return self.get_relation_id(
+            month, self.month_database_id, TARGET_ICON_URL, properties
+        )
+
+    def get_year_relation_id(self, date):
+        year = date.strftime("%Y")
+        start, end = get_first_and_last_day_of_year(date)
+        properties = {"日期": get_date(format_date(start), format_date(end))}
+        return self.get_relation_id(
+            year, self.year_database_id, TARGET_ICON_URL, properties
+        )
+
+    def get_day_relation_id(self, date):
+        day = date.start_of("day").strftime("%Y年%m月%d日")
+        properties = {"日期": get_date(format_date(date.start_of("day")))}
+        return self.get_relation_id(
+            day, self.day_database_id, TARGET_ICON_URL, properties
+        )
+
+    def get_date_relation(self, properties, date):
+        properties["年"] = get_relation(
+            [
+                self.get_year_relation_id(date),
+            ]
+        )
+        properties["月"] = get_relation(
+            [
+                self.get_month_relation_id(date),
+            ]
+        )
+        properties["周"] = get_relation(
+            [
+                self.get_week_relation_id(date),
+            ]
+        )
+        properties["日"] = get_relation(
+            [
+                self.get_day_relation_id(date),
+            ]
+        )
